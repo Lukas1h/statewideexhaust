4b4498cbeb3e3aa994bb5a15a2c24b0eb0286908
Lukas Hahn <lukas1h07@gmail.com>
2024-01-06 16:59:08 -0800

    Make `Card` component respect `className` prop.

--- frontend/app/ui/components/card.tsx
+++ frontend/app/ui/components/card.tsx
@@ -1,11 +1,11 @@
 import { ReactNode } from "react";
 
 export default function Card(
-        {children}:{children:ReactNode}
+        {children, className}:{children:ReactNode, className:string}
     ){
     
     return (
-        <div className="bg-gradient-to-l from-red-100 to-gray-200 rounded-2xl flex items-center flex-col py-6 p-2 md:p-8 mb-4">
+        <div className={`bg-gradient-to-l from-red-100 to-gray-200 rounded-2xl flex items-center flex-col py-6 p-2 md:p-8 mb-4 ${className}`}>
             {children}
         </div>
     )
