f562079faa2c81769ef6fa9f18fd712a3d135b42
Lukas Hahn <lukas1h07@gmail.com>
2026-02-19 23:23:13 -0800

    SEND IT

--- frontend/client.ts
+++
@@ -1,8 +0,0 @@
-// client.ts
-import {createClient} from '@sanity/client'
-
-export default createClient({
-  projectId: 'yuexptbt', // you can find this in sanity.json
-  dataset: 'production', // or the name you chose in step 1
-  useCdn: true // `false` if you want to ensure fresh data
-})
